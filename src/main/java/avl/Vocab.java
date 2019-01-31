3e97febc35713fee9d2f769fa74b8ce81856189a
majeskn <majeskn@wwu.edu>
2019-01-31 15:19:08 -0800

    Added a few things

--- src/main/java/avl/Vocab.java
+++ src/main/java/avl/Vocab.java
@@ -6,14 +6,28 @@ import java.io.FileNotFoundException;
 public class Vocab {
 
     public static void main(String[] args) {
-        //TODO
+        //TODO: no argument case
+        try{
+            Scanner sc = new Scanner(new File(args[0]));
+            Count c = wordCount(sc);
+            System.out.println(c);
+        }catch(FileNotFoundException exc){
+            System.out.println("Could not find file " +args[0]);
+        }
+    
     }
 
     /* count the total and unique words in a document being read
      * by the given Scanner. return the two values in a Count object.*/
     private static Count wordCount(Scanner sc) {
-        //TODO
-        return null;
+        Count c = new Count();
+        while(sc.hasNext()){
+            String word = sc.next();
+            word = word.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+            // TODO: fill in the unique and total fields of c before c is returned
+            //Hint: Something that stores only unique elements to tally # of uniques
+        }
+        return c;
     }
 
 }
