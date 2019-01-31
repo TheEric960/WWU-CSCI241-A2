285b0024ebc3a5bd14ff7fc72503186cf73b5e28
majeskn <majeskn@wwu.edu>
2019-01-31 13:50:51 -0800

    Skeletonized Vocab.java

--- src/main/java/avl/Vocab.java
+++ src/main/java/avl/Vocab.java
@@ -6,30 +6,13 @@ import java.io.FileNotFoundException;
 public class Vocab {
 
     public static void main(String[] args) {
-
-        if (args.length == 0) {
-            Scanner sc = new Scanner(System.in);
-            Count c = wordCount(sc);
-            System.out.println();
-            System.out.println(c);
-        }
-
-        for (String fn : args) {
-            try {
-                File f = new File(fn);
-                Scanner sc = new Scanner(f);
-                Count c = wordCount(sc);
-                System.out.print(fn + " ");
-                System.out.println(c);
-            } catch (FileNotFoundException exc) {
-                System.out.println("Could not find file " + args[0]);
-            }
-        }
+        //TODO
     }
 
     /* count the total and unique words in a document being read
      * by the given Scanner. return the two values in a Count object.*/
     private static Count wordCount(Scanner sc) {
+        //TODO
         return null;
     }
 
