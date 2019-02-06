fa4c9ea80a431143b199aa67c408452a0515a95b
Jack Wilson <wilso313@wwu.edu>
2019-02-06 10:43:32 -0800

    added printTree from lab4 for testing

--- src/main/java/avl/AVL.java
+++ src/main/java/avl/AVL.java
@@ -203,4 +203,21 @@ public class AVL {
         }
     }
 
+    /** print a sideways representation of the tree - root at left,
+     * right is up, left is down. */
+    public void printTree() {
+        printSubtree(root, 0);
+    }
+    private void printSubtree(Node n, int level) {
+        if (n == null) {
+            return;
+        }
+        printSubtree(n.right, level + 1);
+        for (int i = 0; i < level; i++) {
+            System.out.print("        ");
+        }
+        System.out.println(n.word);
+        printSubtree(n.left, level + 1);
+    }
+
 }
