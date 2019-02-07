7470b52b621fea78c30d5d49f6a50e5a2820e548
Jack Wilson <wilso313@wwu.edu>
2019-02-06 19:21:30 -0800

    prints unique words over a fixed-width sliding window for a single file

--- src/main/java/avl/Vocab.java
+++ src/main/java/avl/Vocab.java
@@ -6,21 +6,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 public class Vocab {
 
+    private static int windowSize = 30;
+
     public static void main(String[] args) {
         // no args entered else scan file
         if (args.length == 0) {
             Count c = wordCount(new Scanner(System.in));
             System.out.println(c);
+        } else if (args.length == 1) {
+            findWordCountFromFile(args[0]);
         } else {
             // read every file
             for (String f : args) {
-                try {
-                    Scanner sc = new Scanner(new File(f));
-                    Count c = wordCount(sc);
-                    System.out.println(c);
-                } catch (FileNotFoundException exc) {
-                    System.out.println("Could not find file " + f);
-                }
+                findWordCountFromFile(f);
             }
         }
     }
@@ -29,17 +27,39 @@ public class Vocab {
      * by the given Scanner. return the two values in a Count object.*/
     private static Count wordCount(Scanner sc) {
         Count c = new Count();
+        Count c2 = new Count();
         HashSet<String> set = new HashSet<>();  // doesn't allow duplicates
+        HashSet<String> set2 = new HashSet<>();
 
         while(sc.hasNext()) {
             String word = sc.next();
             word = word.replaceAll("[^a-zA-Z ]", "").toLowerCase();
             set.add(word);
+            set2.add(word);
             c.total += 1;
+            c2.total++;
+            if (c2.total % windowSize == 0) {
+                c2.unique = set2.size();
+                System.out.println(c2);
+                set2 = new HashSet<>();
+                c2.total = 0;
+            }
         }
+        c2.unique = set2.size();
+        System.out.println(c2);
 
         c.unique = set.size();
         return c;
+    }
+
+    private static void findWordCountFromFile(String f) {
+        try {
+            Scanner sc = new Scanner(new File(f));
+            Count c = wordCount(sc);
+            System.out.println("Grand total: " + c);
+        } catch (FileNotFoundException exc) {
+            System.out.println("Could not find file " + f);
+        }
     }
 
 }
